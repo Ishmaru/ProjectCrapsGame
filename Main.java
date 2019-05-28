45c005372e1dc3355436bf5a16fa93abe7f2efb5
Adrian Delpha <adrian.delpha@gmail.com>
2019-05-28 15:29:55 -0700

    Looping gameplay

--- Main.java
+++ Main.java
@@ -3,40 +3,47 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to tech talent south craps");
-		System.out.println("Press '1' to Shoot '2' to quit");
+		System.out.println("Press 's' to Shoot 'q' to quit");
 		
 		DiceRoll dice =  new DiceRoll();
-		int current = 0;
 		
 		Scanner scanObj = new Scanner(System.in);
-		int selection = scanObj.nextInt();
+		String selection = scanObj.nextLine();
 		
-		if(selection == 1) 
+		while(!selection.equals("q") || !selection.equals("Q")) 
 		{
-			int res = dice.initialRoll(dice.rollDice());
-			
-			if(res == 1) 
+			int current = 0;
+			if(selection.equals("s") || selection.equals("S")) 
 			{
-				System.out.println("Win");
-			}
-			else if (res == -1)
-			{
-				System.out.println("Loss");
-			}
-			else
-			{
-				while(current == 0){
-					current = dice.secondaryRoll(dice.rollDice());
-					if(current==-1)
-					{
-						System.out.println("Loss");
-					}
-					if(current==1)
-					{
-						System.out.println("Win");
+				int res = dice.initialRoll(dice.rollDice());
+				
+				if(res == 1) 
+				{
+					System.out.println("Win");
+				}
+				else if (res == -1)
+				{
+					System.out.println("Loss");
+				}
+				else
+				{
+					while(current == 0){
+						current = dice.secondaryRoll(dice.rollDice());
+						if(current==-1)
+						{
+							System.out.println("Loss");
+						}
+						if(current==1)
+						{
+							System.out.println("Win");
+						}
 					}
 				}
 			}
+			dice.point = 0;
+			scanObj = new Scanner(System.in);
+			selection = scanObj.nextLine();
 		}
+
 	}
 }
