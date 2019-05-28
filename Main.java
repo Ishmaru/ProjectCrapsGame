25192693f55789a9ebbecba8f9325221a3b587cc
Adrian Delpha <adrian.delpha@gmail.com>
2019-05-28 16:01:40 -0700

    Place your betsgit add -A

--- Main.java
+++ Main.java
@@ -6,6 +6,7 @@ public class Main {
 		System.out.println("Press 's' to Shoot 'q' to quit");
 		
 		DiceRoll dice =  new DiceRoll();
+		Wallet wallet =  new Wallet(1000.00);
 		
 		Scanner scanObj = new Scanner(System.in);
 		String selection = scanObj.nextLine();
@@ -15,15 +16,22 @@ public class Main {
 			int current = 0;
 			if(selection.equals("s") || selection.equals("S")) 
 			{
+				scanObj = new Scanner(System.in);
+				System.out.println("How much would you like to bet?");
+				double bet = scanObj.nextDouble();
+				wallet.setBet(bet);
+				
 				int res = dice.initialRoll(dice.rollDice());
 				
 				if(res == 1) 
 				{
 					System.out.println("Win");
+					wallet.setMoney(wallet.getBet());
 				}
 				else if (res == -1)
 				{
 					System.out.println("Loss");
+					wallet.setMoney(-wallet.getBet());
 				}
 				else
 				{
@@ -32,15 +40,18 @@ public class Main {
 						if(current==-1)
 						{
 							System.out.println("Loss");
+							wallet.setMoney(-wallet.getBet());
 						}
 						if(current==1)
 						{
 							System.out.println("Win");
+							wallet.setMoney(wallet.getBet());
 						}
 					}
 				}
 			}
 			dice.point = 0;
+			System.out.println("Your current wallet: " + wallet.getMoney());
 			scanObj = new Scanner(System.in);
 			selection = scanObj.nextLine();
 		}
