809105b73a5321542d56ab68eac148a31a255d4b
Adrian Delpha <adrian.delpha@gmail.com>
2019-05-28 11:42:44 -0700

    JUnit Init

--- Main.java
+++ Main.java
@@ -1,8 +1,9 @@
-
+import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		
+		System.out.println("Welcome to tech talent south craps");
+		System.out.println("Press 'S' to Shoot 'Q' to quit");
 	}
 
 }
